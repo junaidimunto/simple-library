a3a34785050fcb555045fa838a8c6da4f7db2664
Junaidi Muntoyib <72388586+junaidimunto@users.noreply.github.com>
2026-09-10 12:02:31 +0700

    Create wilayah.js
    Beberapa fungsi pemanggilan tag html dengan data dinamis untuk pemilihan wilayah di indonesia dari provinsi sampai desa.

---
+++ wilayah.js
@@ -0,0 +1,32 @@
+function selectWilayah ( provinceID, regencyID, districtID, villageID ) {
+
+  const BASE_API_URL = "https://api.kodewilayah.web.id";
+
+  /** Daftar Provinsi */
+  fetch(BASE_API_URL + "/provinces")
+        .then(response => {
+              if (!response.ok) {
+                  throw new Error("Error fetching API data!");
+              }
+              return response.json();
+          })
+          .then(result => {
+
+              const select = document.getElementById(provinceID);
+
+              result.data.forEach(province => {
+                  const option = document.createElement("option");
+
+                  option.value = province.code;
+                  option.textContent = province.name;
+
+                  select.appendChild(option);
+              });
+
+          })
+          .catch(error => {
+              console.error(error);
+              alert("Terjadi kesalahan saat mengambil data provinsi.");
+          });
+
+}
